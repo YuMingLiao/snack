9b614b96365542ad2092d2e0e6ddb392048796ae
YuMingLiao <dycan0716@gmail.com>
2024-09-20 10:11:46 +0000

    Interesting, even I use nix-2.2.2, still argument list too long. So nix-2.2.2 is not the culprit.Then who is?
    notes:
      I could overlay an old nix in an old,non-flake nixpkgs
      by input url tar.gz and ovelay nix = (import old-nixpkgs {}).nix

--- flake.nix
+++ flake.nix
@@ -2,13 +2,16 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs-19-03.url = "https://github.com/NixOS/nixpkgs/archive/80bda4933272f7e244dc9702f39d18433988cdd0.tar.gz";
+    nixpkgs-19-03.flake = false;
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-19-03 }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ overlay ];
         overlay = _: pkgs: {
           packages = pkgs.callPackages nix/packages.nix { };
+          nix = (import nixpkgs-19-03 {}).nix;
         };
 
         pkgs = import nixpkgs { inherit system overlays; };
