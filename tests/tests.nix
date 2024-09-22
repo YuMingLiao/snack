0caf72485f0e4bcbc0657342b8d3065424b4138a
YuMingLiao <dycan0716@gmail.com>
2024-09-22 02:24:33 +0000

    use mkDerivation to write test.nix
    ./test can be executed after setting up builder and src, but snack in derivation still needs recursive-nix.

--- tests/tests.nix
+++ tests/tests.nix
@@ -1,12 +1,12 @@
 { pkgs ? import <nixpkgs> {}}:
 with pkgs;
 let
-  any-paths = derivation {
+  any-paths = stdenv.mkDerivation rec {
     name = "any-paths";
-    builder = "${bash}/bin/bash";
     src = ./any-paths;
-    buildInputs = [ packages.snack-exe ];
-    args = [ any-paths/test ];
-    system = builtins.currentSystem;
-  };
+    builder = ./any-paths/test;
+    nativeBuildInputs = [ packages.snack-exe coreutils nix];
+  }; 
 in any-paths
+
+# failed because testing snack in nix would be a recursive nix.
