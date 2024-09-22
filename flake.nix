75b0e7dc61b302629b852eb3c103a79eb5a428c5
YuMingLiao <dycan0716@gmail.com>
2024-09-22 02:29:37 +0000

    use flake registry nixpkgs in flake inputs
    bug: library-2 library-3 failed at cyclic package name while old snack works.
         packages-2
         template-haskell template-haskell-3
         trans-imp (cycle)

--- flake.nix
+++ flake.nix
@@ -1,13 +1,13 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+#    flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ overlay ];
-        overlay = _: pkgs: {
+        overlay = prev: pkgs: {
           packages = pkgs.callPackages nix/packages.nix { };
         };
 
@@ -15,8 +15,8 @@
       in with pkgs; {
         devShells.default = import ./shell.nix { inherit pkgs; };
         checks = {
-          # my-check = import ./tests/tests.nix { inherit pkgs; };
+          my-check = import ./tests/tests.nix { inherit pkgs; };
         };
-
+        pkgs = pkgs;
       });
 }
