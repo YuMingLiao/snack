9f6fe146dcc346230e4107f6fda786b5ff70141e
YuMingLiao <dycan0716@gmail.com>
2024-09-25 09:53:34 +0000

    fix: seems hGetContent needs LOCALE_ARCHIVE and LANG, which needs HOME=$(pwd). bug: now in Snack.hs libDir tries to download nixpkgs, but in mkDerivation, no internet access.      That also means in default, snack build packages by specsJson, not my nixos-24.05.

--- flake.nix
+++ flake.nix
@@ -10,14 +10,22 @@
         overlay = prev: pkgs: {
           packages = pkgs.callPackages nix/packages.nix { };
         };
-
         pkgs = import nixpkgs { inherit system overlays; };
       in with pkgs; {
-        packages.default = import ./tests/tests.nix { inherit pkgs; };
         devShells.default = import ./shell.nix { inherit pkgs; };
+        packages.default = self.checks.${system}.check-snack-package-file-arg;
         checks = {
           my-check = import ./tests/tests.nix { inherit pkgs; };
+          check-snack-package-file-arg = stdenv.mkDerivation {
+            requiredSystemFeatures = [ "recursive-nix" ];
+            name = "check-snack-package-file-arg";
+            src = ./tests;
+            buildInputs = [packages.snack-exe nix ];
+            LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
+            LANG = "en_US.UTF-8";
+            buildPhase = ''
+              export HOME=$(pwd)
+              snack --package-file ./package.nix run '';};
         };
-        pkgs = pkgs;
       });
 }
