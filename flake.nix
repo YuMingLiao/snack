cf24b5259145ca0139ce6329118cd29d30f4dc39
YuMingLiao <dycan0716@gmail.com>
2024-10-01 01:11:47 +0000

    back to niv and add notes about recursive-nix

--- flake.nix
+++ flake.nix
@@ -21,10 +21,13 @@
       in
       with pkgs;
       {
-        devShells.default = import ./shell.nix { inherit pkgs; };
+        devShells.default = import ./shell.nix {  };
+
         packages.default = self.checks.${system}.check-download;
+
         checks = {
           check-snack-package-file-arg = stdenv.mkDerivation {
+            # for nix-build in snack
             requiredSystemFeatures = [ "recursive-nix" ];
             name = "check-snack-package-file-arg";
             src = ./.;
@@ -32,12 +35,13 @@
               packages.snack-exe
               nix
             ];
+            # for hGetContent in Snack.hs 
             LOCALE_ARCHIVE = "${glibcLocales}/lib/locale/locale-archive";
             LANG = "en_US.UTF-8";
+            # for nix-build in sanck 
             NIX_PATH = "nixpkgs=${nixpkgs}";
             buildPhase = ''
-              export HOME=$(pwd)
-              echo $NIX_PATH
+              HOME=$(pwd)
               cd tests/any-paths/
               snack --package-file ./package.yaml run '';
           };
@@ -46,6 +50,7 @@
             src = ./.;
             requiredSystemFeatures = [ "recursive-nix" ];
             buildInputs = [nix];
+            # for nix/ downloads, also trusted-user and --no-sandbox
             NIX_SSL_CERT_FILE= "/etc/ssl/certs/ca-certificates.crt";
             buildPhase = ''
               HOME=$(pwd)
